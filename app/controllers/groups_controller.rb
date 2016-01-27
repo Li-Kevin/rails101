f162fdb5b6f8f1b462672a74c9c31624feb4f5ce
Li-Kevin <h16861h@gmail.com>
2016-01-27 11:19:08 +0800

    after create group, user become group member auto

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,10 +11,11 @@ class GroupsController < ApplicationController
 	end
 
 	def create
-		@group = Group.create(group_params)
+		@group = current_user.groups.new(group_params)
 
 		if @group.save
-			redirect_to groups_path
+			current_user.join!(@group)
+			redirect_to groups_path, notice: "新增討論版成功"
 		else
 			render :new
 		end
@@ -44,6 +45,32 @@ class GroupsController < ApplicationController
 		@group = Group.find(params[:id])
 		@group.destroy
 		redirect_to groups_path, alert: "討論版已刪除"
+	end
+
+	def join
+		@group = Group.find(params[:id])
+	
+		if !current_user.is_member_of?(@group)
+			current_user.join!(@group)
+			flash[:notice] = "加入本討論版成功！"
+		else
+			flash[:warning] = "你已經是本討論版成員了！"
+		end
+	
+	redirect_to group_path(@group)
+	end
+	
+	def quit
+		@group = Group.find(params[:id])
+	
+		if current_user.is_member_of?(@group)
+			current_user.quit!(@group)
+			flash[:alert] = "已退出本討論版！"
+		else
+			flash[:warning] = "你不是本討論版成員，怎麼退出 XD"
+		end
+	
+	redirect_to group_path(@group)
 	end
 
 	private
