02435330acbf59ff93136af3063428f17d6c15dd
Li-Kevin <h16861h@gmail.com>
2016-01-27 16:06:56 +0800

    handmade to deploy rails project to remote server

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,7 +6,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` on Rails 4+ applications as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = 'dec047e9c827938e14aee71bdd902f6c849087eb39f1876aed92c470eb446af273ef063f1f2060659095802d256dd856008ffcf70f2ff9a4090719a5b109f77c'
+  config.secret_key = 'dec047e9c827938e14aee71bdd902f6c849087eb39f1876aed92c470eb446af273ef063f1f2060659095802d256dd856008ffcf70f2ff9a4090719a5b109f77c'
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
